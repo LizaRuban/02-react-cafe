fbe28808c8614eb40e8feddde770760952bf5b67
Liza Ruban <yelyzaveta.ruban2023@gmail.com>
2026-05-31 15:04:06 +0300

    Add initial files.

---
+++ src/components/App.tsx
@@ -0,0 +1,49 @@
+import css from '../styles/App.module.css';
+import CafeInfo from './CafeInfo.tsx';
+import type { VoteType, Votes } from '../types/votes.ts';
+import { useState } from 'react';
+import VoteOptions from './VoteOptions.tsx';
+import VoteStats from './VoteStats.tsx';
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: VoteType) => {
+    setVotes(prev => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={votes.good > 0 || votes.neutral > 0 || votes.bad > 0}
+      />
+      <VoteStats
+        votes={votes}
+        totalVotes={votes.good + votes.neutral + votes.bad}
+        positiveRate={
+          (votes.good / (votes.good + votes.neutral + votes.bad)) * 100 || 0
+        }
+      />
+    </div>
+  );
+}
+
+export default App;
